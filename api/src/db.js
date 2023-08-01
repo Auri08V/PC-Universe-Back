1522791daf403e01c2e017de16d45cb9766b266e
eduardovalencia <nikivalencia08@gmail.com>
2023-07-31 23:01:04 -0300

    src

---
+++ api/src/db.js
@@ -0,0 +1,14 @@
+// require("dotenv").config()
+// const {Sequelize} = require("sequelize"),
+// const fs = require("fs")
+// const path = require ("path")
+
+// const {
+//     DB_USER, DB_PASSWORD, DB_HOST,
+//   } = process.env;
+  
+//   const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD},${DB_HOST}/pc-universe`,{
+
+//   })
+
+//   const {}
