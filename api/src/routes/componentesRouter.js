ddfdb3b1d63fafd9e6bd4ec9c78d95f694fb0ac1
eduardovalencia <nikivalencia08@gmail.com>
2023-08-03 08:38:08 -0300

    arreglo de rutas

--- api/src/routes/componentesRouter.js
+++ api/src/routes/componentesRouter.js
@@ -1,22 +1,22 @@
-const { Router } = require('express');
+// const { Router } = require('express');
 
-const { getComponentesHandler } = require('../handlers/componentesHandlers')
+// const { getComponentesHandler } = require('../handlers/componentesHandlers').default
 
-const componentesRouter = Router();
+// const componentesRouter = Router();
 
-componentesRouter.get("/", getComponentesHandler);
+// componentesRouter.get("/", getComponentesHandler);
 
-componentesRouter.get("/:id", async (req, res) => {
-    const id  = req.params.id
-    const allComponentes = await getAllComponentes()
-    try {
-        if (id) {
-            const componenteId = await allComponentes.find(componente => componente.id == (id))
-            componenteId ? res.status(200).send(componenteId) : res.status(404).json("Componente no encontrado");
-        }
-    } catch (error) {
-        return res.status(404).send(error.message)
-    }
-});
+// componentesRouter.get("/:id", async (req, res) => {
+//     const id  = req.params.id
+//     const allComponentes = await getAllComponentes()
+//     try {
+//         if (id) {
+//             const componenteId = await allComponentes.find(componente => componente.id == (id))
+//             componenteId ? res.status(200).send(componenteId) : res.status(404).json("Componente no encontrado");
+//         }
+//     } catch (error) {
+//         return res.status(404).send(error.message)
+//     }
+// });
 
-module.exports = componentesRouter;
+// module.exports = componentesRouter;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,16 +1,12 @@
-const {Router} = require ("express");
-const componentesRouter = require('./componentesRouter');
+const { Router } = require("express");
+// const componentesRouter = require('./componentesRouter');
+const getDataByNameHandler = require("../handlers/componentesHandlers");
+const getAllData = require("../handlers/componentesHandlers");
+const getDataByIdController = require("../Controllers/componentesController");
+const router = Router();
 
-const DB = require('../../DB.json')
-
-const router = Router ()
-
-
-// router.use("/componentes", (req, res)=> {
-//     res.json(DB);
-// });
-
-router.use("/componentes", componentesRouter);
-
-
+// router.use("/componentes", componentesRouter);
+router.use("/componentes/name", getDataByNameHandler.getDataByNameHandler);
+router.use("/componentes", getAllData.getAllData);
+router.use("/componentes/:id", getDataByIdController.getDataByIdController);
 module.exports = router;
