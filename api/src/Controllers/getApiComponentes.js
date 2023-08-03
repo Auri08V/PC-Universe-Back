ddb061d0e3739cd690319f604da3ee1b0a45fb9e
eduardovalencia <nikivalencia08@gmail.com>
2023-08-03 08:33:11 -0300

    arreglos de para buscar por nombre y para poder traer todos los comoponetes

--- api/src/Controllers/getApiComponentes.js
+++ api/src/Controllers/getApiComponentes.js
@@ -1,103 +1,118 @@
-const DB = require('../../DB.json');
+const DB = require("../../DB.json");
 
 const getApiComponentes = async () => {
-    const infoGabinetes = DB.gabinetes.map(gabinete => {
-        return {
-            modelo: gabinete.modelo,
-            especificaciones: gabinete.especificaciones,
-            precio: gabinete.precio,
-            img: gabinete.img,
-            categoria: gabinete.categoria
-        };
-
-    });
-    const infoMemoriasRam = DB.memorias_ram.map(memoriaRam => {
-        return {
-            modelo: memoriaRam.modelo,
-            especificaciones: memoriaRam.especificaciones,
-            precio: memoriaRam.precio,
-            img: memoriaRam.img,
-            categoria: memoriaRam.categoria
-        };
-    });
-    const infoPlacasGraficas = DB.placas_graficas.map(placaGrafica => {
-        return {
-            modelo: placaGrafica.modelo,
-            especificaciones: placaGrafica.especificaciones,
-            precio: placaGrafica.precio,
-            img: placaGrafica.img,
-            categoria: placaGrafica.categoria
-        };
-    });
-    const infoMonitores = DB.monitores.map(monitor => {
-        return {
-            modelo: monitor.modelo,
-            especificaciones: monitor.especificaciones,
-            precio: monitor.precio,
-            img: monitor.img,
-            categoria: monitor.categoria
-        };
-    });
-    const infoDiscos = DB.discos.map(disco => {
-      return {
-          modelo: disco.modelo,
-          especificaciones: disco.especificaciones,
-          precio: disco.precio,
-          img: disco.img,
-          categoria: disco.categoria
-      };
+  const infoGabinetes = DB.gabinetes.map((gabinete) => {
+    return {
+      id: gabinete.id,
+      modelo: gabinete.modelo,
+      especificaciones: gabinete.especificaciones,
+      precio: gabinete.precio,
+      img: gabinete.img,
+      categoria: gabinete.categoria,
+    };
   });
-    const infoFuentesDePoder = DB.fuentes_de_poder.map(fuenteDePoder => {
-        return {
-            modelo: fuenteDePoder.modelo,
-            especificaciones: fuenteDePoder.especificaciones,
-            precio: fuenteDePoder.precio,
-            img: fuenteDePoder.img,
-            categoria: fuenteDePoder.categoria
-        };
-    });
-    const infoProcesadores = DB.procesadores.map(procesador => {
-        return {
-            modelo: procesador.modelo,
-            especificaciones: procesador.especificaciones,
-            precio: procesador.precio,
-            img: procesador.img,
-            categoria: procesador.categoria
-        };
-    });
-    const infoPlacasBase= DB.placas_madres.map(placaMadre => {
-        return {
-            modelo: placaMadre.modelo,
-            especificaciones: placaMadre.especificaciones,
-            precio: placaMadre.precio,
-            img: placaMadre.img,
-            categoria: placaMadre.categoria
-        };
-    });
-    const infoRefrigerantes = DB.refrigerantes.map(refrigerante => {
-        return {
-            modelo: refrigerante.modelo,
-            especificaciones: refrigerante.especificaciones,
-            precio: refrigerante.precio,
-            img: refrigerante.img,
-            categoria: refrigerante.categoria
-        };
-    });
-    const infoPerifericos = DB.perifericos.map(periferico => {
-        return {
-            tipo: periferico.tipo,
-            modelo: periferico.modelo,
-            especificaciones: periferico.especificaciones,
-            precio: periferico.precio,
-            img: periferico.img,
-            categoria: periferico.categoria
-        };
-    });
-    const allComponentes = [...infoGabinetes, ...infoMemoriasRam, ...infoPlacasGraficas, ...infoMonitores, ...infoDiscos, ...infoFuentesDePoder, ...infoProcesadores, ...infoPlacasBase, ...infoRefrigerantes, ...infoPerifericos];
+  const infoMemoriasRam = DB.memorias_ram.map((memoriaRam) => {
+    return {
+      id: memoriaRam.id,
+      modelo: memoriaRam.modelo,
+      especificaciones: memoriaRam.especificaciones,
+      precio: memoriaRam.precio,
+      img: memoriaRam.img,
+      categoria: memoriaRam.categoria,
+    };
+  });
+  const infoPlacasGraficas = DB.placas_graficas.map((placaGrafica) => {
+    return {
+      id: placaGrafica.id,
+      modelo: placaGrafica.modelo,
+      especificaciones: placaGrafica.especificaciones,
+      precio: placaGrafica.precio,
+      img: placaGrafica.img,
+      categoria: placaGrafica.categoria,
+    };
+  });
+  const infoMonitores = DB.monitores.map((monitor) => {
+    return {
+      id: monitor.id,
+      modelo: monitor.modelo,
+      especificaciones: monitor.especificaciones,
+      precio: monitor.precio,
+      img: monitor.img,
+      categoria: monitor.categoria,
+    };
+  });
+  const infoDiscos = DB.discos.map((disco) => {
+    return {
+      id: disco.id,
+      modelo: disco.modelo,
+      especificaciones: disco.especificaciones,
+      precio: disco.precio,
+      img: disco.img,
+      categoria: disco.categoria,
+    };
+  });
+  const infoFuentesDePoder = DB.fuentes_de_poder.map((fuenteDePoder) => {
+    return {
+      modelo: fuenteDePoder.modelo,
+      especificaciones: fuenteDePoder.especificaciones,
+      precio: fuenteDePoder.precio,
+      img: fuenteDePoder.img,
+      categoria: fuenteDePoder.categoria,
+    };
+  });
+  const infoProcesadores = DB.procesadores.map((procesador) => {
+    return {
+      modelo: procesador.modelo,
+      especificaciones: procesador.especificaciones,
+      precio: procesador.precio,
+      img: procesador.img,
+      categoria: procesador.categoria,
+    };
+  });
+  const infoPlacasBase = DB.placas_madres.map((placaMadre) => {
+    return {
+      modelo: placaMadre.modelo,
+      especificaciones: placaMadre.especificaciones,
+      precio: placaMadre.precio,
+      img: placaMadre.img,
+      categoria: placaMadre.categoria,
+    };
+  });
+  const infoRefrigerantes = DB.refrigerantes.map((refrigerante) => {
+    return {
+      modelo: refrigerante.modelo,
+      especificaciones: refrigerante.especificaciones,
+      precio: refrigerante.precio,
+      img: refrigerante.img,
+      categoria: refrigerante.categoria,
+    };
+  });
+  const infoPerifericos = DB.perifericos.map((periferico) => {
+    return {
+      tipo: periferico.tipo,
+      modelo: periferico.modelo,
+      especificaciones: periferico.especificaciones,
+      precio: periferico.precio,
+      img: periferico.img,
+      categoria: periferico.categoria,
+    };
+  });
+  const allComponentes = [
+    ...infoGabinetes,
+    ...infoMemoriasRam,
+    ...infoPlacasGraficas,
+    ...infoMonitores,
+    ...infoDiscos,
+    ...infoFuentesDePoder,
+    ...infoProcesadores,
+    ...infoPlacasBase,
+    ...infoRefrigerantes,
+    ...infoPerifericos,
+  ];
 
-    return allComponentes;
-}
-    
+  return allComponentes;
+};
+
 module.exports = {
-    getApiComponentes
-}
+  getApiComponentes,
+};
--- api/src/Controllers/getDbComponentes.js
+++ api/src/Controllers/getDbComponentes.js
@@ -1,41 +1,41 @@
-const { Componentes, Perifericos } = require('../db');
-const getDbPerifericos = async () => {
-    try {
-        const dbPerifericos = await Perifericos.bulkCreate({
-            raw: true,
-            nest: true,
-        });
+// const { Componentes, Perifericos } = require('../db');
+// const getDbPerifericos = async () => {
+//     try {
+//         const dbPerifericos = await Perifericos.bulkCreate({
+//             raw: true,
+//             nest: true,
+//         });
 
-        return dbPerifericos;
-    } catch (error) {
-        console.error('Error al obtener componentes de la base de datos:', error);
-        return [];
-    }
-};
+//         return dbPerifericos;
+//     } catch (error) {
+//         console.error('Error al obtener componentes de la base de datos:', error);
+//         return [];
+//     }
+// };
 
-const getDbComponentes = async () => {
-    try {
-        const dbComponentes = await Componentes.bulkCreate({
-            raw: true,
-            nest: true,
-        });
+// const getDbComponentes = async () => {
+//     try {
+//         const dbComponentes = await Componentes.bulkCreate({
+//             raw: true,
+//             nest: true,
+//         });
 
-        return dbComponentes;
-    } catch (error) {
-        console.error('Error al obtener componentes de la base de datos:', error);
-        return [];
-    }
-};
+//         return dbComponentes;
+//     } catch (error) {
+//         console.error('Error al obtener componentes de la base de datos:', error);
+//         return [];
+//     }
+// };
 
-const getDbAll = async () => {
-    const dataFrontComponentes = await getDbComponentes();
-    const dataFrontPerifericos = await getDbPerifericos();
+// const getDbAll = async () => {
+//     const dataFrontComponentes = await getDbComponentes();
+//     const dataFrontPerifericos = await getDbPerifericos();
 
-    const dataAll = dataFrontComponentes.concat(dataFrontPerifericos);
+//     const dataAll = dataFrontComponentes.concat(dataFrontPerifericos);
 
-    return dataAll
-};
+//     return dataAll
+// };
 
-module.exports = {
-    getDbAll,
-};
+// module.exports = {
+//     getDbAll,
+// };
