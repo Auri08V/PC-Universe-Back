76dd1f9f7cd3c2027d5b812d628995e7a34c8ee3
ManuelGalan <mafagahe@gmail.com>
2023-08-05 00:49:59 -0500

    modificacion en componetesController, componetesHandlers, componentesRouter

--- api/src/Controllers/componentesController.js
+++ api/src/Controllers/componentesController.js
@@ -1,76 +1,20 @@
-// const { Op } = require("sequelize");
-// const {Componentes} = require ("../db")
-// const { getAllData } = require("./getApiComponentes");
-
-// const getAllComponentes = async () => {
-//   try {
-//     const apiComponentes = await getAllData();
-//     const allComponentes = apiComponentes;
-//     return allComponentes;
-//   } catch (error) {
-//     console.error("Error al obtener todos los componentes:", error);
-//     console.error("Error en la ruta componentesController.js");
-//     return [];
-//   }
-// };
-
-// const getDataByNameController = async (name) => {
-//   const allData = await getAllData();
-
-//   if (name) {
-//     const lowerCaseName = name.toLowerCase();
-//     const filteredData = allData.filter((data) =>
-//       data.modelo.toLowerCase().includes(lowerCaseName)
-//     );
-
-//     if (filteredData.length === 0) {
-//       const dataLike = allData.filter((data) =>
-//         data.modelo.toLowerCase().includes(lowerCaseName)
-//       );
-//       return dataLike.length ? dataLike : null;
-//     } else {
-//       return filteredData;
-//     }
-//   } else {
-//     return allData;
-//   }
-// };
-
-// const getDataByIdController = async (req, res) => {
-//     const { id } = req.params;
-//     try {
-//       const response = await Componentes.findOne({
-//         where:
-//           { id: id }
-//       });
-//       res.status(200).json(response);
-//     } catch (error) {
-//       res.status(400).json({ error: error.message });
-//     }
-//   };
-
-
-
-
-// module.exports = {
-//   getAllComponentes,
-//   getDataByNameController,
-//   getDataByIdController,
-// };
-
-
 const { Op } = require("sequelize");
 // const { getDbAll } = require("./getDbComponentes");
 const {Componentes} = require ("../db")
 const { getAllData } = require("./getApiComponentes");
 
-const getAllComponentes = async () => {
+
+const getAllComponentes = async (page = 1, itemsPerPage = 8) => {
   try {
     const apiComponentes = await getAllData();
-    const allComponentes = apiComponentes;
-    return allComponentes;
+    
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const paginatedComponentes = apiComponentes.slice(startIndex, endIndex);
+
+    return paginatedComponentes;
   } catch (error) {
-    console.error("Error al obtener todos los componentes:", error);
+    console.error("Error al obtener los componentes:", error);
     console.error("Error en la ruta componentesController.js");
     return [];
   }
@@ -84,15 +28,9 @@ const getDataByNameController = async (name) => {
     const filteredData = allData.filter((data) =>
       data.modelo.toLowerCase().includes(lowerCaseName)
     );
+    const filter8 = filteredData.slice(0,7)
+    return filter8
 
-    if (filteredData.length === 0) {
-      const dataLike = allData.filter((data) =>
-        data.modelo.toLowerCase().includes(lowerCaseName)
-      );
-      return dataLike.length ? dataLike : null;
-    } else {
-      return filteredData;
-    }
   } else {
     return allData;
   }
@@ -113,22 +51,6 @@ const getDataByIdController = async (req, res) => {
     res.status(500).send("Error al buscar por ID");
   }
 }
-// try {
-//   const {id} = req.params;
-//   const response = await getAllComponentes.findAll({
-//     where : {
-//       name:id
-//     }
-    
-//   })
-//   console.log(response);  
-//   res.status(200).json(response)
-// } catch (error) {
-//   res.status(400).json({error:error.messaje})
-// }
-
-// };
-
 
 module.exports = {
   getAllComponentes,
