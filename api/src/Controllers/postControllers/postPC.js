87840aa86460440810b7a506a5abdc5e9f18914e
pablo perez <perezpablo0903mail.com>
2023-08-08 12:48:52 -0300

    pcfinal returns

--- api/src/Controllers/postControllers/postPC.js
+++ api/src/Controllers/postControllers/postPC.js
@@ -1,36 +1,37 @@
-const { PcFinal, Componentes, Perifericos } = require('../../db');
+const { PcFinal, Componentes, Perifericos } = require('../db');
 
 const postPC = async (req, res) => {
     try {
-       
-        const { nombre, precio_total, componentes, perifericos } = req.body;
- 
-        const componentesNombres = await Componentes.findAll({
-            where: { id: componentes },
-            attributes: ["modelo"],
-        });
+        // Extraer las propiedades necesarias del body
+        const { precio_total, componentes } = req.body;
 
-        const perifericosNombres = await Perifericos.findAll({
-            where: { id: perifericos },
-            attributes: ["modelo"],
-        });
+        // Extraer los datos de los componentes
+        const componenteIds = [];
+        const modelos = [];
+        const imgs = [];
+        const precios = [];
 
-        const componentesyperifericos = {
-            componentes: componentesNombres.map((componente) => componente.modelo),
-            perifericos: perifericosNombres.map((periferico) => periferico.modelo),
-        };
+        for (const componente of componentes) {
+            componenteIds.push(componente.id);
+            modelos.push(componente.modelo);
+            imgs.push(componente.img);
+            precios.push(componente.precio);
+        }
 
+        // Crear la PcFinal en la base de datos
         const pcFinal = await PcFinal.create({
-            nombre,
             precio_total,
-            especificaiones: JSON.stringify(componentesyperifericos),
+            componenteId: componenteIds,
+            modelo: modelos,
+            img: imgs,
+            precio: precios,
         });
 
         // Respuesta exitosa
-        return res.status(201).json({ success: true, message: "Pc creada exitosamente." });
+        return res.status(201).json({ success: true, message: "PcFinal creada exitosamente." });
     } catch (error) {
-        console.error("Error al crear la Pc:", error);
-        return res.status(500).json({ success: false, message: "Error al crear la Pc." });
+        console.error("Error al crear la PcFinal:", error);
+        return res.status(500).json({ success: false, message: "Error al crear la PcFinal." });
     }
 };
 module.exports = postPC;
