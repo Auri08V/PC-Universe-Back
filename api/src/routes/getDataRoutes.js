f3423a9815d1824248cdadabdcf021fcc2005cd5
Eduardo Valencia <116929726+Auri08V@users.noreply.github.com>
2023-08-09 15:42:28 -0300

    Delete getDataRoutes.js

--- api/src/routes/getDataRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const {
-  getAllDataControllers,
-} = require("../Controllers/getControllers/getAllDataControllers");
-const createComponenteRoute = Router();
-createComponenteRoute.get("/", getAllDataControllers);
-module.exports = createComponenteRoute;
-
