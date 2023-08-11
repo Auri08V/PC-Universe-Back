51706672acc320de28ef76eee37f5a651ed29d87
pablo perez <perezpablo0903mail.com>
2023-08-11 14:50:29 -0300

    carhito funcional

--- api/src/routes/paymentRoutes.js
+++ api/src/routes/paymentRoutes.js
@@ -3,5 +3,7 @@ const createOrder = require('../Controllers/paymentControllers/payment');
 const payment = Router();
 
 payment.post("/payment", createOrder);
-
+payment.get('/success', (req, res) => { res.redirect("/http://127.0.0.1:5173/componentes") })
+payment.get('/failure', (req, res) => { res.redirect("/http://127.0.0.1:5173/componentes") })
+payment.get('/pending', (req, res) => { res.redirect("/http://127.0.0.1:5173/componentes") })
 module.exports = payment;
