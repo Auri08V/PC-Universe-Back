125ae642f36045748217bdccea7c4da9c0524e18
pablo perez <perezpablo0903mail.com>
2023-08-19 21:28:39 -0300

    ingresos

--- api/src/Controllers/getControllers/getPaymentsRecord.js
+++ api/src/Controllers/getControllers/getPaymentsRecord.js
@@ -8,7 +8,15 @@ const getPaymentsRecord = async (req, res) => {
 
         if (!response.length === 0) return res.status(404).json({ error: "Records not found" })
 
-        res.status(200).json(response)
+        const amountTotal = response.reduce((total, record) => total + record.amount, 0);
+
+        const responseData = {
+            response,
+            amountTotal
+        }
+
+
+        res.status(200).json(responseData)
     } catch (error) {
         res.status(500).json({ error: "Error in getPaymentsRecord.js" })
     }
