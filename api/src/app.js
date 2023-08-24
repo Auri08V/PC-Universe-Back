c602a9ff5acb0990581e5835ae5fe6591d4e4f53
Eduardo Valencia <116929726+Auri08V@users.noreply.github.com>
2023-08-24 10:41:15 -0300

    la pute

--- api/src/app.js
+++ api/src/app.js
@@ -65,7 +65,7 @@ server.use("/", newProduct)
 
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://pc-universe.vercel.app/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
